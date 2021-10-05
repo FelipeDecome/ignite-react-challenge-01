44370340e268e7e54eef3d5f1a60098c209c16de
Felipe Decome <decome.felipe@gmail.com>
2021-10-05 15:38:34 -0300

    challenge complete

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,20 +10,31 @@ interface Task {
   isComplete: boolean;
 }
 
+const guid = () => Math.floor(Math.random() * Date.now());
+
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    setTasks((state) => [...state, {
+      id: guid(),
+      title: newTaskTitle,
+      isComplete: false,
+    }])
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks((state) => state.map(task => ({
+      ...task,
+      isComplete: task.id === id ? !task.isComplete : task.isComplete,
+    })));
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks((state) => state.filter((task) => task.id !== id));
   }
 
   return (
